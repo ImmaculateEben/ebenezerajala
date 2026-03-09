4bc2efe9606a32edb3ba88799a340e21fa15edb8
Ebenezer Ajala <ebenezerajala1305@gmail.com>
2026-03-09 13:07:27 +0100

    Add .cpanel.yml and deploy portfolio

--- portfolio/assets/js/runtime-config.js
+++ portfolio/assets/js/runtime-config.js
@@ -1,9 +1,9 @@
 window.EA_RUNTIME_CONFIG = {
-  supabase: {
-    url: "https://rsawwbmgbcknuqpueioc.supabase.co",
-    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJzYXd3Ym1nYmNrbnVxcHVlaW9jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2NDYwMzgsImV4cCI6MjA4ODIyMjAzOH0.lQXtlu-krAEureNpjMLWuaTSe5nIQfQOnZP5TCdIRsI"
+  "supabase": {
+    "url": "https://rsawwbmgbcknuqpueioc.supabase.co",
+    "anonKey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJzYXd3Ym1nYmNrbnVxcHVlaW9jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2NDYwMzgsImV4cCI6MjA4ODIyMjAzOH0.lQXtlu-krAEureNpjMLWuaTSe5nIQfQOnZP5TCdIRsI"
   },
-  adminEmail: "ebenezerajala1305@gmail.com",
-  storageBucket: "portfolio-assets",
-  turnstileSiteKey: ""
+  "adminEmail": "ebenezerajala1305@gmail.com",
+  "storageBucket": "portfolio-assets",
+  "turnstileSiteKey": "0x4AAAAAACoTvDWAYFrQY6-L"
 };
